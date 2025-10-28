1c14e9ba41dd22c4a5033483259524fe6b54f414
Oleg <olgerd2002@mail.ru>
2025-10-28 22:49:50 +0300

    commit 2025-10-28 v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ObjectContainer container = new ObjectContainer();
+        container.add(2);
+        container.add(1);
+        container.add(3);
+
+        System.out.println(container);
         }
 
 }
--- src/main/java/org/example/ObjectContainer.java
+++ src/main/java/org/example/ObjectContainer.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class ObjectContainer {
+    private Object[] elements;
+    private int size;
+
+    public ObjectContainer() {
+        this.elements = new Object[10];
+        this.size = 0;
+    }
+
+    public void add(Object element) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size++] = element;
+    }
+
+    private void resize() {
+        Object[] newArray = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        elements = newArray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i < size - 1) sb.append(", ");
+        }
+        return sb.append("]").toString();
+    }
 }
